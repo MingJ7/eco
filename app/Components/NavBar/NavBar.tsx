a77c19e6bfffd9802c7c8a5e1453cf857be743f2
Ming <m>
2025-03-09 02:22:22 +0800

    remove margin for mobile

--- app/Components/NavBar/NavBar.tsx
+++ app/Components/NavBar/NavBar.tsx
@@ -3,14 +3,15 @@ import NavBarItem from './NavBarItem'
 
 export default function NavBar() {
   return (
-    <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left bg-green-200">
+    <div className="grid text-center grid-cols-1 lg:mb-0 lg:grid-cols-4 lg:text-left bg-green-200">
         <h1 className='text-4xl font-bold'>
           Eco
         </h1>
         <NavBarItem
-          url="/order"
+          url="/order/dish"
           header='Order'
         />
+        <div/>
         <NavBarItem
           url="/login"
           header='Login'
